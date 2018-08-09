5f63f01c4d6d10bbc0cf151c21f28e51f84da140
johnhaus <john.haus@outlook.com>
2018-08-09 17:10:19 -0300

    added some view and routes stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-resources : cocktails, only: [:index, :show, :new, :create]
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
 
